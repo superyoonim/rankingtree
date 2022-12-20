14a84bb855298c10a6975a523cba34ff3465b4bb
yoo <woow3aa@naver.com>
2022-12-20 17:07:32 +0900

    평점 변화와 그밖에 등등 수정

--- src/routes/ProductPageDetails.js
+++ src/routes/ProductPageDetails.js
@@ -1,9 +1,31 @@
 import './ProductPageDetails.css';
+import { FaHeart } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaShareSquare } from "react-icons/fa";
+
+import ImageGallery from 'react-image-gallery';
+
+
+const images = [
+  { id:1 ,
+    original : 'https://picsum.photos/id/1018/1000/600/',
+    title: '배경1'
+  },
+  { id:2 ,
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    title: '배경2'
+  },
+  // etc.
+];
 
 const ProductPageDetails = () =>  {
 
 return (
+
+   
+
     <section className='MainCenter'>
+
         <div className='MainBox'>
             <div className='screen1'>
              <div className='screen1-1'>LikeLion NFT Project</div>
@@ -12,7 +34,8 @@ return (
              <ul>통계를 낼 수 있는 정보가 부족합니다</ul></div>
              <div className='screen1-3'></div>
              <div className='screen1-4'>
-                Leaf |   일반적인 유저들의 평점입니다
+             
+               Leaf |   일반적인 유저들의 평점입니다
                 <ul>통계를 낼 수 있는 정보가 부족합니다</ul>
              </div>
             </div>
@@ -25,13 +48,16 @@ return (
             <div className='screen2-2'></div>
             <div className='screen2-3'>
             <div className='screen2-3-1'>
-            <img src="images/starIcon.png" alt="starIcon" />
+                <FaStar size="20"/> 즐겨찾기
+            {/* <img src="images/starIcon.png" alt="starIcon" /> */}
             </div>
             <div className='screen2-3-2'>
-            <img src="images/heartIcon.png" alt="heartIcon" />
+                <FaHeart size="20"/> 좋아요
+            {/* <img src="images/heartIcon.png" alt="heartIcon" /> */}
             </div>
             <div className='screen2-3-3'>
-            <img src="images/shareIcon.png" alt="shareIcon" />
+                <FaShareSquare size="20"/> 공유하기
+            {/* <img src="images/shareIcon.png" alt="shareIcon" /> */}
             </div>
             </div>
 
@@ -47,20 +73,34 @@ return (
                 <div className='floor1-1'>by
                 <div className='normalFont'>  (주)멋쟁이 사자처럼</div>
                 </div>
-                <div className='floor1-1'>Chain
-                <div className='normalFont'>  *Klaytn</div>
+                <div className='floor1-1'>Chain  
+                <div className='normalFont'>  Klaytn</div>
                 </div>
-                <div className='floor1-1'>Links
-                <div className='normalFont'>  *</div>
+                <div className='floor1-1'>Links 
+                <div className='normalFont'> </div>
                 </div>
             </div>
             
    
             </div>
+   
             <div className='SubBoxRight'>
-                {/* <div className='topBox1'>__________멋쟁이 사자처럼 메인 사진</div>
-                <div className='topBox2'>__________사진 더보기</div> */}
-            <img src="images/subRight.png" alt="subRight" />
+                <div className='topBox1'>
+                <ImageGallery
+        items={images}
+        showFullscreenButton={false}
+        showThumbnails={false}
+      />
+      <style>
+        {`.image-gallery-image {
+          width: 50%;
+          height: auto !important;
+        }`}
+      </style>
+                </div>
+                <div className='topBox2'>
+     </div>
+            {/* <img src="images/subRight.png" alt="subRight" /> */}
             </div>
         </section>
         <div className='iconPlus'>
@@ -89,14 +129,27 @@ return (
             <div className='CloseBoxLeft'> 
             Rate your Impression
             <br/>
-            <img src="images/Leaf.png" alt="Leaf" />
+            <form className="mb-3" name="myform" id="myform" method="post">
+        <fieldset>
+        
+          <input type="radio" name="reviewStar" defaultValue={5} id="rate1" /><label htmlFor="rate1">♠</label>
+          <input type="radio" name="reviewStar" defaultValue={4} id="rate2" /><label htmlFor="rate2">♠</label>
+          <input type="radio" name="reviewStar" defaultValue={3} id="rate3" /><label htmlFor="rate3">♠</label>
+          <input type="radio" name="reviewStar" defaultValue={2} id="rate4" /><label htmlFor="rate4">♠</label>
+          <input type="radio" name="reviewStar" defaultValue={1} id="rate5" /><label htmlFor="rate5">♠</label>
+        </fieldset>
+      
+      </form>
+            {/* <img src="images/Leaf.png" alt="Leaf" /> */}
+
+            
             </div>
             <div className='CloseBoxRight'>
             Comments
-            <br/>
+            <br/><br/>
             <input 
             type="text" 
-            className='IputBoxPro'
+            className='InputBoxPro'
             value="댓글을 입력해주세요" />
             <button
             className='ButtonBoxPro'>등록</button>
@@ -105,5 +158,7 @@ return (
     </section>
 )
 }
+
+
 
 export default ProductPageDetails;
